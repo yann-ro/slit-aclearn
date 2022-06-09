64564d5851ca0ea0462f7ff517662e50386d0d63
YannRodriguez <52059549+YannRodriguez@users.noreply.github.com>
2022-06-09 08:31:23 +0200

    update

--- aclearn/dataset.py
+++ aclearn/dataset.py
@@ -1,25 +1,26 @@
-from matplotlib.transforms import Transform
+from sklearn.model_selection import train_test_split
 from torchvision.datasets import MNIST,FashionMNIST
+from matplotlib.transforms import Transform
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
 import numpy as np
 
 
 class AcLearnDataset():
-    def __init__(self, dataset_path='demo_mnist', size_init_per_class=1):
+    def __init__(self, data_path, label_path=None, data_path_unlab=None, size_init_per_class=1):
         """
         """
 
-        if (dataset_path=='demo_mnist') or (dataset_path=='demo_fmnist'): 
-            train, test, self.nb_class, self.image_size = self.read_famous_dataset(dataset_path)
+        if (data_path=='demo_mnist') or (data_path=='demo_fmnist'): 
+            self.read_famous_dataset(data_path)
         else:
-            train, test, self.nb_class, self.image_size = self.read_dataset(dataset_path)
+            self.read_dataset(data_path, label_path, data_path_unlab, extension='npy')
         
-        self.X_train, self.y_train = train
-        self.X_test, self.y_test = test
-
+        if not data_path_unlab:
+            self.init_label_pool(size_init_per_class)
+        
         self.preprocessing()
-        self.init_label_pool(size_init_per_class)
+
 
 
 
@@ -38,22 +39,37 @@ class AcLearnDataset():
         traindataloader = DataLoader(train, shuffle=True, batch_size=60000)
         testdataloader  = DataLoader(test , shuffle=True, batch_size=10000)
         
-        X_train, y_train = next(iter(traindataloader))
-        X_train, y_train = X_train.numpy(), y_train.numpy()
-        X_test , y_test = next(iter(testdataloader))
-        X_test , y_test = X_test.numpy() , y_test.numpy()
+        self.X_train, self.y_train = next(iter(traindataloader))
+        self.X_train, self.y_train = self.X_train.numpy(), self.y_train.numpy()
+        self.X_test , self.y_test = next(iter(testdataloader))
+        self.X_test , self.y_test = self.X_test.numpy() , self.y_test.numpy()
 
-        nb_class = len(set(y_test))
-        image_size = X_train.shape[2:]
-
-        return (X_train, y_train), (X_test, y_test), nb_class, image_size
+        self.nb_class = len(set(self.y_test))
+        self.image_size = self.X_train.shape[-2:]
     
 
-    def read_dataset(self, path):
+
+    def read_dataset(self, data_path, label_path, data_path_unlab, extension='npy'):
         """
         """
 
-        #return X_train, y_train, X_test, y_test, nb_class, image_size
+        if extension == 'npy':
+            X = np.load(data_path).reshape(-1, 28,28)
+            y = np.array(np.load(label_path).squeeze(), dtype=int)
+
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2)
+
+            if data_path_unlab:
+                self.X_pool = np.load(data_path_unlab)
+                self.y_pool = None
+
+            self.nb_class = len(set(y))
+            self.image_size = X.shape[-2:]
+
+        else :
+            print('ERROR: File extension not supported')
+        
+
 
 
     def preprocessing(self):
@@ -61,7 +77,9 @@ class AcLearnDataset():
         """
         self.X_train = self.X_train.reshape(self.X_train.shape[0], 1, self.image_size[0], self.image_size[1])
         self.X_test = self.X_test.reshape(self.X_test.shape[0], 1, self.image_size[0], self.image_size[1])
-    
+
+        self.X_pool = self.X_pool.reshape(self.X_pool.shape[0], 1, self.image_size[0], self.image_size[1])
+
     
 
     def init_label_pool(self,nb_init_label_per_class):
