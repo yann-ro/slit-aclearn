d5073202798f996b7bd84d61802a7054c99b6c9b
YannRodriguez <52059549+YannRodriguez@users.noreply.github.com>
2022-06-21 09:24:40 +0200

    update

--- aclearn/model.py
+++ aclearn/model.py
@@ -253,7 +253,7 @@ def plot_results(X_train, y_train, X_selected, y_selected, X_pool, tsne=None, pc
         x2d_selected_tsne = tsne.transform(X_selected)
         x2d_pool_tsne = tsne.transform(X_pool)
         
-        print(x2d_train_tsne.shape)
+        print(x2d_train_tsne)
         
         confidence_ellipse(x2d_train_tsne, y_train, ax[0], n_std=2, cm=cm.magma, alpha=0.4)
         
