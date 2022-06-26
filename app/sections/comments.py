61de6c3cd2e49d14d2251e3bb7466ec07381b085
YannRodriguez <52059549+YannRodriguez@users.noreply.github.com>
2022-06-26 21:28:01 +0200

    update

--- app/sections/comments.py
+++ app/sections/comments.py
@@ -3,18 +3,23 @@ import streamlit as st
 def display_explanation(kind):
     text = '...'
 
+    #sampling
     if kind=='sampling_random':
         text = 'Sampling randomly/uniform correspond to select randomly new samples. It correspond to baseline sampling strategy in active learning.'
 
     if kind=='sampling_var_ratio':
         text = ''
             
-    
     if kind=='sampling_bald':
         text = ''
     
-    if kind=='max_entropy':
+    if kind=='sampling_max_entropy':
         text = ''
     
+    #algorithms
+    if kind=='algo_mc_dropout':
+        text = 'Prediction incertitude quantified using dropout as Bayesian estimator of weights. <https://arxiv.org/abs/1506.02142>'
+
+
     with st.expander("See explanation"): 
         st.markdown(text, unsafe_allow_html=True)
