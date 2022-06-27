93aa39ff43543cf20178142d91b538ef75d4e56a
YannRodriguez <52059549+YannRodriguez@users.noreply.github.com>
2022-06-27 13:31:35 +0200

    update

--- app/sections/comments.py
+++ app/sections/comments.py
@@ -6,16 +6,16 @@ def display_explanation(kind):
 
     #sampling
     if kind=='sampling_random':
-        text = 'Sampling randomly/uniform correspond to select randomly new samples. It correspond to baseline sampling strategy in active learning.'
+        text = 'Sampling randomly/uniform correspond to select randomly new points. It correspond to baseline sampling strategy in active learning.'
 
     if kind=='sampling_var_ratio':
-        text = ''
+        text = 'Sampling points that would minimize output variance, which is one of the components of error.'
             
     if kind=='sampling_bald':
-        text = ''
+        text = 'Sampling points that are expected to maximise the information gained about the model parameters, i.e. maximise the mutual information between predictions and model posterior. <https://arxiv.org/abs/1112.5745>'
     
     if kind=='sampling_max_entropy':
-        text = ''
+        text = 'Sampling points that maximise the predictive entropy'
     
     #algorithms
     if kind=='algo_mc_dropout':
